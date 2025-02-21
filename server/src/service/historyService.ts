d235cd8f3f4c7fa594813323c3ba0e3f9031c0c6
Maggie Ellinwood <mellinwood1023@gmail.com>
2025-02-20 22:40:28 -0500

    index.html route

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -14,7 +14,9 @@ class City {
 class HistoryService implements City {
   
   // TODO: Define a read method that reads from the searchHistory.json file
- private async read() {}
+ private async read() {
+  const filePath = path.join(__dirname, './data/searchHistory.json');
+ }
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
 private async write(cities: City[]) {}
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
